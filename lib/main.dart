5138a5c1227b9342c0581219256109f081f57a71
JH <wogks27@naver.com>
2023-09-05 16:51:53 +0900

    Feat: data fetching

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:webt/screens/home_screen.dart';
+import 'package:webt/services/api_service.dart';
 
 void main() {
+  ApiService().getTodaysToons();
   runApp(const MyApp());
 }
 
